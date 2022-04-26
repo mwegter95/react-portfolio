8aeef9d2204f8d4f29e32179bd13c80862d3fcec
Michael W Macbook <mwegter95@gmail.com>
2022-04-25 23:00:23 -0500

    added resume embedded to resume page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,7 +15,9 @@ function Home(props) {
         <ProjectList />
         </div>
       ): currentCategory.name === "Resume" ? ( 
-      <div>this is Resume</div>
+      <div>This is my Resume. Notice the well-done group projects, good education, and experience working effectively in a corporate setting.
+        <iframe title="resume" src="https://drive.google.com/file/d/1iCy7a3TzraxLrjEDzoASztCBI5gwueqO/preview" width="100%" height="960" allow="autoplay"></iframe>
+      </div>
       ): currentCategory.name === "About Me" ? (
       <div>this is about me</div>
     ):(
