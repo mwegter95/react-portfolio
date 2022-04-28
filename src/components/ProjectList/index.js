3d3dd4bd14b6f299cdfd938d61e31a650cb44a70
Michael W Macbook <mwegter95@gmail.com>
2022-04-27 23:45:43 -0500

    added masonry layout to portfolio page. Big win

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import projects from '../../db/projects';
-import Card from '../Card'
+import Card from '../Card';
+import Masonry from 'react-masonry-css';
+
 
 
 
 const ProjectList = () => {
 
   return (
-      <div className="flex-row">
+      <Masonry breakpointCols={3} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
         {projects.map((project, i) => (
-          <Card key={i} item={project}/>
+          <Card key={i} item={project} />
         ))}
-      </div>
+      </Masonry>
   );
 };
 
