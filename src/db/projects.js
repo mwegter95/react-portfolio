bbdfccfb189ef3011a0118da635ed99c348a6ab3
Michael Wegter <michael.wegter@turnberrysolutions.com>
2022-08-21 15:32:02 -0400

    added some images

--- src/db/projects.js
+++ src/db/projects.js
@@ -1,21 +1,23 @@
-import buzzstockimage from '../assets/images/buzzstock_screenshot.png'
+import buzzStockImage from '../assets/images/buzzstock_screenshot.png';
+import myWeddingImage from '../assets/images/my_wedding_login_page.jpg';
+
 
 const projects = [
     {
       name: 'BuzzStock',
       category: 'coding',
       description: 'BuzzStock is an app that combines information from a news api and a stock info api, so it can show news and pricing for a searched ticker symbol',
-      image: buzzstockimage,
+      image: buzzStockImage,
       alt: "screenshot of BuzzStock app",
-      link: "https://github.com/mwegter95/deep-thoughts"
+      link: "https://github.com/TavianJD/BuzzStock-Group-Project/"
     },
     {
       name: 'My Wedding',
       category: 'coding',
       description:
         'My Wedding allows users to keep a day-of schedule for their wedding; it uses MySQL, Sequelize, and Handlebars',
-      image: {},
-      alt: "screenshot of My Wedding",
+      image: myWeddingImage,
+      alt: "screenshot of My Wedding login page",
       link: "https://github.com/GitAndrewCoffee/wedding_planner"
     },
     {
@@ -24,7 +26,7 @@ const projects = [
       description:
         'Deep Thoughts is a backend-only MongoDB project for a social app where users can share thoughts, and comment on other thoughts from different users',
       image: {},
-      alt: "screenshot of Tempo homepage",
+      alt: "screenshot of Deep Thoughts api requests and responses",
       link: "https://github.com/mwegter95/deep-thoughts"
     }
   ];
