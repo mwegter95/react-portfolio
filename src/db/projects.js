f928daf49e3e632efb4b3943d88a00b135786705
Michael Wegter <michael.wegter@turnberrysolutions.com>
2022-08-21 15:19:57 -0400

    added links to some projects. Next is images

--- src/db/projects.js
+++ src/db/projects.js
@@ -4,21 +4,28 @@ const projects = [
     {
       name: 'BuzzStock',
       category: 'coding',
-      description:
-        'BuzzStock is an app that combines information from a news api and a stock info api, so it can show news and pricing for a searched ticker symbol',
-        image: buzzstockimage,
+      description: 'BuzzStock is an app that combines information from a news api and a stock info api, so it can show news and pricing for a searched ticker symbol',
+      image: buzzstockimage,
+      alt: "screenshot of BuzzStock app",
+      link: "https://github.com/mwegter95/deep-thoughts"
     },
     {
       name: 'My Wedding',
       category: 'coding',
       description:
-        'My Wedding allows users to keep a day-of schedule for their wedding; it uses MySQL, Sequelize, and Handlebars'
+        'My Wedding allows users to keep a day-of schedule for their wedding; it uses MySQL, Sequelize, and Handlebars',
+      image: {},
+      alt: "screenshot of My Wedding",
+      link: "https://github.com/GitAndrewCoffee/wedding_planner"
     },
     {
       name: 'Deep Thoughts',
       category: 'coding',
       description:
-        'Deep Thoughts is a backend-only MongoDB project for a social app where users can share thoughts, and comment on other thoughts from different users'
+        'Deep Thoughts is a backend-only MongoDB project for a social app where users can share thoughts, and comment on other thoughts from different users',
+      image: {},
+      alt: "screenshot of Tempo homepage",
+      link: "https://github.com/mwegter95/deep-thoughts"
     }
   ];
 
